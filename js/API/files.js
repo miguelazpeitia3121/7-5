bf1e12b953a33eaf551c4678cddf2c12a9274f2c
Miguel Alvarez <miguelazpeitia@yahoo.com>
2013-01-11 20:46:50 -0600

    7-5

---
+++ js/API/files.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", function(){
+		$('#archivos .rounded li').tap(function(){
+			switch($(this).index()){
+				case 0://Escribir (Crear) Archivos
+					
+					break;
+				case 1://Leer Archivo
+					
+			}
+		});
+	});
+});
